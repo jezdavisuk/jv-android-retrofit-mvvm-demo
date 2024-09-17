1d8bd700f06f8b6ea6565bf24f0dd6914baa2b45
Jez <jezdavisuk@gmail.com>
2024-09-17 12:18:51 +0100

    feat: incorporate Retrofit builder functionality to method RetrofitInstance.getService()

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/RetrofitInstance.java
@@ -1,6 +1,7 @@
 package com.northcoders.jv_android_retrofit_mvvm_demo.service;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
 
@@ -9,7 +10,12 @@ public class RetrofitInstance {
     private static final String BASE_URL = "https://fruityvice.com/api/fruit/";
 
     public static ApiService getService() {
-        assert retrofit == null;
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
 
     }
 
