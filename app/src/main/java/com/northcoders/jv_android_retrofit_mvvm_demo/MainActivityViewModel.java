f926d7f0e644166b751d53689af95340ce5d077d
Jez <jezdavisuk@gmail.com>
2024-09-17 15:34:55 +0100

    feat: pass Application context to constructor, embed within instance of class FruitRepository

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/MainActivityViewModel.java
@@ -1,7 +1,19 @@
 package com.northcoders.jv_android_retrofit_mvvm_demo;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.northcoders.jv_android_retrofit_mvvm_demo.model.FruitRepository;
+
 public class MainActivityViewModel extends AndroidViewModel {
+
+    private FruitRepository fruitRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.fruitRepository = new FruitRepository(application);
+    }
 
 }
