8fd9946f199a79d9bc3c83eb273e490dcc985b56
Jez <jezdavisuk@gmail.com>
2024-09-17 11:42:51 +0100

    feat: add class Fruit to package model

---
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/model/Fruit.java
@@ -0,0 +1,26 @@
+package com.northcoders.jv_android_retrofit_mvvm_demo.model;
+
+public class Fruit {
+    private String name;
+    private Long id;
+    private String family;
+    private String order;
+    private String genus;
+    private NutritionProfile nutritions;
+
+    public Fruit() {
+
+    }
+
+    public Fruit(String name, Long id, String family, String order, String genus, NutritionProfile nutritions) {
+        this.name = name;
+        this.id = id;
+        this.family = family;
+        this.order = order;
+        this.genus = genus;
+        this.nutritions = nutritions;
+    }
+
+
+
+}
