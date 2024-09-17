ccd4665e018781aaef61fd063fc74c90cac4940e
Jez <jezdavisuk@gmail.com>
2024-09-17 12:34:02 +0100

    feat: add method getAllFruits to interface ApiService returns a Call object of List<Fruit>

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/ApiService.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/ApiService.java
@@ -1,4 +1,15 @@
 package com.northcoders.jv_android_retrofit_mvvm_demo.service;
 
+import com.northcoders.jv_android_retrofit_mvvm_demo.model.Fruit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface ApiService {
+
+    @GET
+    Call<List<Fruit>> getAllFruits();
+
 }
