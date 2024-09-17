4290ffa3d388c5869dd0b0bfc093e41de10ec93b
Jez <jezdavisuk@gmail.com>
2024-09-17 15:26:43 +0100

    feat: add method getMutableLiveData() to class FruitRepository

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/model/FruitRepository.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/model/FruitRepository.java
@@ -1,12 +1,21 @@
 package com.northcoders.jv_android_retrofit_mvvm_demo.model;
 
 import android.app.Application;
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
+import com.northcoders.jv_android_retrofit_mvvm_demo.service.ApiService;
+import com.northcoders.jv_android_retrofit_mvvm_demo.service.RetrofitInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 import java.util.List;
+import java.util.Objects;
 
 public class FruitRepository {
 
-    private MutableLiveData<List<Fruit>> mtld = new MutableLiveData<>();
+    private MutableLiveData<List<Fruit>> mutableLiveData = new MutableLiveData<>();
     private Application application;
 
     public FruitRepository(Application application) {
@@ -14,6 +23,22 @@ public class FruitRepository {
     }
 
     public MutableLiveData<List<Fruit>> getMutableLiveData() {
+        ApiService apiService = RetrofitInstance.getService();
+        Call<List<Fruit>> call = apiService.getAllFruits();
 
+        call.enqueue(new Callback<List<Fruit>>() {
+            @Override
+            public void onResponse(Call<List<Fruit>> call, Response<List<Fruit>> response) {
+                List<Fruit> fruits = response.body();
+                mutableLiveData.setValue(fruits);
+            }
+
+            @Override
+            public void onFailure(Call<List<Fruit>> call, Throwable throwable) {
+                Log.i("HTTP Failure", Objects.requireNonNull(throwable.getMessage()));
+            }
+        });
+
+        return mutableLiveData;
     }
 }
