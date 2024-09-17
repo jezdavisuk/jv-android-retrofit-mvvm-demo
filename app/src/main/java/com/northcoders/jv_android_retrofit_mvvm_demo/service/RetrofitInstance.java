4ad5235960231eda5ed3a28cb5ecc6ebf01868b1
Jez <jezdavisuk@gmail.com>
2024-09-17 12:29:59 +0100

    feat: add HTTP logging implementation to method RetrofitInstance.getService()

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/RetrofitInstance.java
@@ -1,5 +1,7 @@
 package com.northcoders.jv_android_retrofit_mvvm_demo.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,10 +12,17 @@ public class RetrofitInstance {
     private static final String BASE_URL = "https://fruityvice.com/api/fruit/";
 
     public static ApiService getService() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
 
