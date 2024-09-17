c13608d98f31c2d25ae99d12d02c5d10a1bd6b5b
Jez <jezdavisuk@gmail.com>
2024-09-17 15:38:43 +0100

    feat: add method getAllFruit() to class MainActivityViewModel

--- app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/MainActivityViewModel.java
@@ -4,8 +4,12 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 
+import com.northcoders.jv_android_retrofit_mvvm_demo.model.Fruit;
 import com.northcoders.jv_android_retrofit_mvvm_demo.model.FruitRepository;
+
+import java.util.List;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
@@ -16,4 +20,7 @@ public class MainActivityViewModel extends AndroidViewModel {
         this.fruitRepository = new FruitRepository(application);
     }
 
+    public LiveData<List<Fruit>> getAllFruit() {
+        return fruitRepository.getMutableLiveData();
+    }
 }
