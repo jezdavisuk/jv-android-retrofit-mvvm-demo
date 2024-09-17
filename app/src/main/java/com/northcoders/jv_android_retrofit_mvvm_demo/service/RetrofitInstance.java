5935d0137af0aed8bf31af964ea3c9771dd330f3
Jez <jezdavisuk@gmail.com>
2024-09-17 12:08:59 +0100

    feat: add class RetrofitInstance contains static instance of class Retrofit and static variable BASE_URL

---
+++ app/src/main/java/com/northcoders/jv_android_retrofit_mvvm_demo/service/RetrofitInstance.java
@@ -0,0 +1,12 @@
+package com.northcoders.jv_android_retrofit_mvvm_demo.service;
+
+import retrofit2.Retrofit;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+
+    private static final String BASE_URL = "https://fruityvice.com/api/fruit/";
+
+
+}
